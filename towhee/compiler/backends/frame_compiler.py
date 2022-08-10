109009fc76b336f8bb34e6f9bf496d38b50d61ff
Reiase <reiase@gmail.com>
2022-08-10 08:02:42 +0800

    add torch frame compiler
    Signed-off-by: Reiase <reiase@gmail.com>

---
+++ towhee/compiler/backends/frame_compiler.py
@@ -0,0 +1,28 @@
+from types import FrameType
+import weakref
+from torchdynamo.guards import GuardedCode
+
+
+class _Tracker(dict):
+    def add(self, strong_obj):
+        idx = id(strong_obj)
+        if id(strong_obj) not in self:
+            self[idx] = weakref.ref(strong_obj, lambda _: self.pop(idx))
+
+    def __contains__(self, item):
+        return dict.__contains__(self, id(item))
+
+    @property
+    def seen(self):
+        return list(self.values())
+
+
+class FrameCompiler:
+    input_codes = _Tracker()
+    output_codes = _Tracker()
+
+    def __init__(self, cache_size: int) -> None:
+        pass
+
+    def __call__(self, frame: FrameType) -> GuardedCode:
+        pass
