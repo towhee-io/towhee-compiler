4a6660f38951cec946f4474225a3adce9a785f40
reiase <reiase@gmail.com>
2022-08-13 19:54:39 +0800

    fix unit test problems
    Signed-off-by: reiase <reiase@gmail.com>

--- tests/unittests/test_numba_frontend.py
+++ tests/unittests/test_numba_frontend.py
@@ -19,12 +19,43 @@ def inner_distance(query, data):
     return dists
 
 
+def inner_distance_kwargs(query, data, bias=0.0):
+    dists = []
+    for vec in data:
+        dist = bias
+        for i in range(len(vec)):
+            dist += vec[i] * query[i]
+        dists.append(dist)
+    return dists
+
+
+class test:
+    @staticmethod
+    def inner_distance(query, data):
+        dists = []
+        for vec in data:
+            dist = 0
+            for i in range(len(vec)):
+                dist += vec[i] * query[i]
+            dists.append(dist)
+        return dists
+
+    def my_inner_distance(self, query, data):
+        dists = []
+        for vec in data:
+            dist = 0
+            for i in range(len(vec)):
+                dist += vec[i] * query[i]
+            dists.append(dist)
+        return dists
+
+
 class TestNumbaFrontend(TestCase):
-    # TODO: add unit test for function with kwargs
-    # TODO: add unit test for function with args and kwargs
+    # DONE: add unit test for function with kwargs
+    # DONE: add unit test for function with args and kwargs
     # TODO: add unit test for instance method
     # TODO: add unit test for class method
-    # TODO: add unit test for static method
+    # DONE: add unit test for static method
     def setUp(self) -> None:
         self.data = [np.random.random((10000, 128)) for _ in range(10)]
         self.query = np.random.random(128)
@@ -41,6 +72,23 @@ class TestNumbaFrontend(TestCase):
         with jit_compile(feature=True):
             result = inner_distance(self.x1, self.x2)
         self.assertListEqual(result, [14, 32])
+
+    def test_numba_compile_with_kwargs(self):
+        with jit_compile(feature=True):
+            result = inner_distance_kwargs(self.x1, self.x2, bias=1.0)
+        self.assertListEqual(result, [15.0, 33.0])
+
+    def test_numba_compile_static_method(self):
+        with jit_compile(feature=True):
+            result = test.inner_distance(self.x1, self.x2)
+        self.assertListEqual(result, [14, 32])
+
+    # TODO: fix the test case for instance method
+    # def test_numba_compile_instance_method(self):
+    #     with jit_compile(feature=True):
+    #         t = test()
+    #         result = t.my_inner_distance(self.x1, self.x2)
+    #     self.assertListEqual(result, [14, 32])
 
     def test_numba_benchmark(self):
         t1 = time.time()
