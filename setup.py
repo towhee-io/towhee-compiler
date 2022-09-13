5d473ffd2eeba5f9854b9b46ab5237768c139c69
shiyu22 <shiyu.chen@zilliz.com>
2022-09-13 17:37:05 +0800

    Update setup
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 from typing import List
+from pathlib import Path
 
 from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
 from torch.utils.cpp_extension import CppExtension
 
-long_description = """
-Towhee compiler is a Python JIT compiler that
-speeds up AI-related codes by native code generation.
-The project is inspired by [Numba](https://github.com/numba/numba),
-[Pyjion](https://www.trypyjion.com) and [TorchDynamo](https://github.com/pytorch/torchdynamo).
-Towhee compiler uses a frame evaluation hook (see [PEP 523]: https://www.python.org/dev/peps/pep-0523/)
-to get the chance of compiling python bytecodes into native code.
-"""
+HERE = Path(__file__).parent
+README = (HERE / 'README.md').read_text()
 
 def parse_requirements(file_name: str) -> List[str]:
-    with open(file_name) as f:
+    with open(HERE / file_name) as f:
         return [
             require.strip() for require in f
             if require.strip() and not require.startswith('#')
@@ -27,7 +22,7 @@ setup(
     version="0.1.0",
     url="https://github.com/towhee-io/towhee-compiler",
     description="A JIT compiler for accelerating AI programs written in python.",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     author='Towhee Team',
     author_email='towhee-team@zilliz.com',
